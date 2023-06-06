84ffc36adcb5e069688b4a8d4f5e3af2e0c77ecf
Will C <wirewc@gmail.com>
2023-06-06 13:35:38 -0400

    Iteration: Testing, documentation, and versioning  (#2)
    Co-authored-by: zamaz <71521611+zachariahmiller@users.noreply.github.com>

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -2,9 +2,9 @@ resource "random_id" "default" {
   byte_length = 2
 }
 
-module kms {
+module "kms" {
   source = "../../"
 
-  kms_key_description = "Test KMS key."
+  kms_key_description       = "Test KMS key."
   kms_key_alias_name_prefix = "${var.kms_key_alias_name_prefix}-${lower(random_id.default.hex)}"
 }
